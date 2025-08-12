460e5157ab1436ff833b522d0cfed1b8e10dc697
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-08-12 12:57:10 -0600

    feat(backend): add middleware to app.js
    - express.json
    - express.urlencoded
    - cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: false }));
+// Allow cross-origin requests (frontend <-> backend)
+app.use(cors());
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Homepage");
