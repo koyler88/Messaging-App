849ad34323b6df4df34f02fe031e84ea1e20aca9
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-08-12 10:58:25 -0600

    feat(backend): create basic express app

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
