c0fdcf233cb65ee0d99e09db02cd260b6d15f971
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-08-14 16:22:43 -0600

    feat(frontend): add initial route structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
-  return (
-    <div>Welcome to the Homepage!</div>
-  )
-}
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Profile from "./pages/Profile.jsx";
+import Messages from "./pages/Messages.jsx";
 
-export default App
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/messages" element={<Messages />} />
+    </Routes>
+  );
+}
